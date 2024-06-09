ea357a58d7180614a900105617f4a1c2a02d7347
Thibaut Durand <durand.tibo@gmail.com>
2024-06-09 13:13:36 -0700

    Add functions to configure records

--- src/minrecord/__init__.py
+++ src/minrecord/__init__.py
@@ -6,8 +6,11 @@ __all__ = [
     "BaseComparator",
     "MaxScalarComparator",
     "MinScalarComparator",
+    "get_max_size",
+    "set_max_size",
 ]
 
+from minrecord._config import get_max_size, set_max_size
 from minrecord.comparator import (
     BaseComparator,
     MaxScalarComparator,
