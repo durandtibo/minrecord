9cd507091936e19e50c0155a3fdb975b0ec10cdb
Thibaut Durand <durand.tibo@gmail.com>
2024-06-09 10:31:59 -0700

    Add comparators (#7)

--- src/minrecord/__init__.py
+++ src/minrecord/__init__.py
@@ -1,1 +1,15 @@
 r"""Root package."""
+
+from __future__ import annotations
+
+__all__ = [
+    "BaseComparator",
+    "MaxScalarComparator",
+    "MinScalarComparator",
+]
+
+from minrecord.comparator import (
+    BaseComparator,
+    MaxScalarComparator,
+    MinScalarComparator,
+)
