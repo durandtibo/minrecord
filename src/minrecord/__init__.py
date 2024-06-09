84170f3a55a6d09b14c64df9915c03eadc5e1bab
Thibaut Durand <durand.tibo@gmail.com>
2024-06-09 13:32:07 -0700

    Add record base class (#10)

--- src/minrecord/__init__.py
+++ src/minrecord/__init__.py
@@ -4,13 +4,17 @@ from __future__ import annotations
 
 __all__ = [
     "BaseComparator",
+    "BaseRecord",
+    "EmptyRecordError",
     "MaxScalarComparator",
     "MinScalarComparator",
+    "NotAComparableRecordError",
     "get_max_size",
     "set_max_size",
 ]
 
 from minrecord._config import get_max_size, set_max_size
+from minrecord.base import BaseRecord, EmptyRecordError, NotAComparableRecordError
 from minrecord.comparator import (
     BaseComparator,
     MaxScalarComparator,
